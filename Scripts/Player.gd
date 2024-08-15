2bfe15879cd6d4def951e549dadbb3658ef3f924
Joao Pedro Miranda Salim <joaopedromirandasalim@gmail.com>
2024-08-15 11:58:56 -0300

    Adiciona explosão

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,10 +1,16 @@
 extends CharacterBody2D
 
 @onready var sprite : AnimatedSprite2D = $AnimatedSprite2D
-var bomba : PackedScene = preload("res://Objetos/bomba.tscn")
-const SPEED = 100.0
 @onready var tilemap : TileMapLayer = $"../CaixasQuebraveis"
 
+var bomba : PackedScene = preload("res://Objetos/bomba.tscn")
+const SPEED = 100.0
+
+var tile : Vector2i
+var vivo = true
+
+func _ready() -> void:
+	GameController.tomouDanoNoTile.connect(verificaSeTomouDano)
 
 func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
@@ -12,6 +18,7 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_vector("Esquerda", "Direita", "Cima", "Baixo")
 	velocity = direction.normalized() * SPEED
 	
+	tile = GameController.local_to_map(global_position)
 	updateSprite()
 	move_and_slide()
 
@@ -27,8 +34,16 @@ func updateSprite():
 		sprite.flip_h = true
 
 func _unhandled_input(event: InputEvent) -> void:
+	if !vivo: return 
 	if event.is_action_pressed("Espaco"):
 		var b : Node2D = bomba.instantiate()
-		var tile : Vector2i = tilemap.local_to_map(global_position)
-		b.global_position = tilemap.map_to_local(tile)
+		var tile : Vector2i = GameController.local_to_map(global_position)
+		b.global_position = GameController.map_to_local(tile)
 		get_tree().root.add_child(b)
+
+func verificaSeTomouDano(pos : Vector2i):
+	if pos == tile:
+		set_physics_process(false)
+		vivo = false
+		sprite.play("Morrer")
+		
