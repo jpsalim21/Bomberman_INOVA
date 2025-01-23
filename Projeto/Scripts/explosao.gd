635afc5e0851088306b8fac0378d9e6950039fbe
João Pedro Miranda Salim <joaopedromirandasalim@gmail.com>
2025-01-23 19:22:01 -0300

    Tira as coisas já feitas

--- Projeto/Scripts/explosao.gd
+++
@@ -1,6 +0,0 @@
-extends AnimatedSprite2D
-
-#Queremos que a explosão saia da cena quando terminar animação. Logo, damos
-#queue_free (deletar) quando a animação termina
-func _ready() -> void:
-	animation_finished.connect(queue_free)
--- Projeto/Scripts/wallReference.gd
+++
@@ -1,7 +0,0 @@
-extends TileMapLayer
-
-
-# Esse script só serve para avisarmos ao GameController quais os tiles
-# Não estão livres
-func _ready() -> void:
-	GameController.tilemapReferencia = self as TileMapLayer
